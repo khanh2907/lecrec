b1b71eaf9fde55bbe7a38e00bfaf856bf0a3f180
Khanh Nguyen <khanh@intersect.org.au>
2014-08-15 21:23:13 +1000

    Add roles to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  has_and_belongs_to_many :roles
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def is_admin?
+  	roles.include?(Role.find_by_name("Administrator"))
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def set_role(email, role)
+  user = User.find_by_email(email)
+  role = Role.find_by_name(role)
+  user.roles << role
+  user.save!
+end
+
+Role.delete_all
+Role.create!(:name => "Administrator")
+Role.create!(:name => "Student")
+Role.create!(:name => "Tutor")
+puts 'CREATED ROLES'
+
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+set_role(user.email, "Administrator")
